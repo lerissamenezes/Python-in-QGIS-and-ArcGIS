8f61e04390c81d2e07e594fb2ed082ccf2869db1
ler <lerissamenezes01@gmail.com>
2025-04-29 08:56:15 +0200

    Exercise 2

---
+++ Exercise2.py
@@ -0,0 +1,31 @@
+
+def donuts(count):
+    print("Number of donuts: ")
+    if count>=10:
+        print("many")
+    else:
+        print(count)
+    return
+def verbing(s):
+ #+++yourcodehere+++
+ return
+def remove_adjacent(nums):
+ #+++yourcodehere+++
+ return
+def main():
+    print('donuts')
+    print(donuts(4))
+    print(donuts(9))
+    print(donuts(10))
+    print(donuts('twentyone'))
+    print('verbing')
+    print(verbing('hail'))
+    print(verbing('swiming'))
+    print(verbing('do'))
+    print('remove_adjacent')
+    print(remove_adjacent([1,2,2,3]))
+    print(remove_adjacent([2,2,3,3,3]))
+    print(remove_adjacent([]))
+ #Standardboilerplatetocallthemain()function.
+if __name__=='__main__':
+    main()
